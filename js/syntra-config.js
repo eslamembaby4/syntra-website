be2ecf84756445bc838b3d1c4817798198be8314
eslamembaby4 <77410377+eslamembaby4@users.noreply.github.com>
2026-01-14 01:23:24 -0400

    Updated syntra-config.js

--- js/syntra-config.js
+++ js/syntra-config.js
@@ -13,11 +13,11 @@
 window.SYNTRA_CONFIG = {
   // Your Supabase project URL
   // Example: https://abcdefghijklmnop.supabase.co
-  supabaseUrl: 'https://woailjjdiamgvahcxnrj.supabase.co',
+  supabaseUrl: 'https://0ec90b57d6e95fcbda19832f.supabase.co',
 
   // Your Supabase anon/public key (safe to use in client-side code)
   // Example: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...
-  supabaseKey: 'sb_publishable_-z87K81PBPmGNHHpyLlsWg_ii6jMHd1'
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJib2x0IiwicmVmIjoiMGVjOTBiNTdkNmU5NWZjYmRhMTk4MzJmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg4ODE1NzQsImV4cCI6MTc1ODg4MTU3NH0.9I8-U0x86Ak8t2DGaIk0HfvTSLsAyzdnz-Nw00mMkKw'
 };
 
 console.log('[Syntra Config] Configuration loaded:', {
